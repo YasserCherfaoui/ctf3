e86c0606e86c5eca82f182707e606c0e428f8002
yassercher <yasser.cher42@gmail.com>
2023-03-29 23:03:08 +0100

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,18 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import '@/styles/Home.module.css';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAk8pGEwSHue8TFuFaTVqHKGavvPh9Lv_s",
+  authDomain: "cloudauth00.firebaseapp.com",
+  projectId: "cloudauth00",
+  storageBucket: "cloudauth00.appspot.com",
+  messagingSenderId: "63355610177",
+  appId: "1:63355610177:web:9127ea94a08b6910b0d2c0",
+  measurementId: "G-XZ56D8JXWP"
+};
+
+initializeApp(firebaseConfig);
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,37 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import React from 'react';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-const inter = Inter({ subsets: ['latin'] })
+const auth = getAuth();
 
 export default function Home() {
+  
+  const [email, setEmail] = React.useState(null);
+  const [passwd, setPasswd] = React.useState(null);
+  
+  const handleChange = (event) => {
+    event.preventDefault();
+    console.log(event);
+    if(event.target.id == 'email'){
+      setEmail(event.target.value);
+    }else{
+      setPasswd(event.target.value);
+    }
+    console.log(`Email: ${email} password:${passwd}`);
+  }
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    createUserWithEmailAndPassword(auth,email,passwd).then(
+      (userCredentials) => {
+        const user = userCredentials.user;
+        console.log('User create successfully ' + user);
+      }
+    ).catch(
+      (e) => {
+        console.error(e);
+      }
+    )
+  }
   return (
     <>
       <Head>
@@ -14,108 +40,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+      <main>
+        <div className='sign-in'>
+          <h1>Sign in</h1>
+          <label htmlFor='email'>Email</label>
+          <input type={'email'} onChange={handleChange}  id='email' placeholder={'yourname@email.com'} required />
+          <label htmlFor='passwd'>Password</label>
+          <input type={'password'} onChange={handleChange} id='passwd' placeholder={'࡞࡞࡞࡞࡞࡞࡞'} required />  
+          <button onClick={handleSignIn}>Sign In</button>
         </div>
       </main>
     </>
